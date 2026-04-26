11ce88744f70c717cb32d05965cf7f29c8899aed
vinicius <vinicius.mponte@gmail.com>
2026-04-26 14:10:02 -0300

    feat: adição do CardVertical

---
+++ app/src/main/java/unicsul/pdm/cruzeiroecosul/componentes/CardVertical.java
@@ -0,0 +1,33 @@
+package unicsul.pdm.cruzeiroecosul.componentes;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.imageview.ShapeableImageView;
+
+import unicsul.pdm.cruzeiroecosul.R;
+
+public class CardVertical extends MaterialCardView {
+
+    public CardVertical(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        inflate(context, R.layout.card_vertical, this);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CardVertical);
+
+        ((ShapeableImageView) findViewById(R.id.cardVerticalImagem))
+                .setImageResource(a.getResourceId(R.styleable.CardVertical_verticalImagem, 0));
+        ((TextView) findViewById(R.id.cardVerticalTitulo))
+                .setText(a.getString(R.styleable.CardVertical_verticalTitulo));
+        ((TextView) findViewById(R.id.cardVerticalDescricao))
+                .setText(a.getString(R.styleable.CardVertical_verticalDescricao));
+        ((TextView) findViewById(R.id.cardVerticalChip))
+                .setText(a.getString(R.styleable.CardVertical_verticalChip));
+
+        a.recycle();
+    }
+}
