436f0a7f0f4fa27ac86bc2fae66f51503d4cfeaf
vinicius <vinicius.mponte@gmail.com>
2026-04-29 18:39:23 -0300

    feat: adição de cores no chip

--- app/src/main/java/unicsul/pdm/cruzeiroecosul/componentes/CardVertical.java
+++ app/src/main/java/unicsul/pdm/cruzeiroecosul/componentes/CardVertical.java
@@ -2,8 +2,12 @@ package unicsul.pdm.cruzeiroecosul.componentes;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.util.AttributeSet;
 import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.imageview.ShapeableImageView;
@@ -22,11 +26,18 @@ public class CardVertical extends MaterialCardView {
         TextView titulo = findViewById(R.id.cardVerticalTitulo);
         TextView descricao = findViewById(R.id.cardVerticalDescricao);
         TextView chip = findViewById(R.id.cardVerticalChip);
+        GradientDrawable chipBackground = (GradientDrawable) chip.getBackground();
 
         imagem.setImageResource(a.getResourceId(R.styleable.CardVertical_imagem, 0));
         titulo.setText(a.getString(R.styleable.CardVertical_titulo));
         descricao.setText(a.getString(R.styleable.CardVertical_descricao));
         chip.setText(a.getString(R.styleable.CardVertical_chip));
+
+        int corChip = a.getColor(R.styleable.CardVertical_corChip, ContextCompat.getColor(context, R.color.chip));
+        int corTextoChip = a.getColor(R.styleable.CardVertical_corTextChip, ContextCompat.getColor(context, R.color.black));
+
+        chipBackground.setColor(corChip);
+        chip.setTextColor(corTextoChip);
 
         a.recycle();
     }
